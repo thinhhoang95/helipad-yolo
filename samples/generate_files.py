53bcc96d73b654e4b88e6150fced56c4c4e89c9d
Thinh Hoang <hdinhthinh@gmail.com>
2020-07-16 14:31:43 +0700

    Path fix

--- samples/generate_files.py
+++ samples/generate_files.py
@@ -1,4 +1,4 @@
 with open('helipad.txt', 'a') as file:
     for i in range(1,230,1):
         filename = '{:03d}'.format(i)
-        file.write('images/img_' + filename + '.jpg\n')
+        file.write('samples/images/img_' + filename + '.jpg\n')
