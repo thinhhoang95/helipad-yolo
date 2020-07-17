2cbe9353f15e31d14fbb144acace0df50e7bcbf2
Thinh Hoang <thinhhoang@usercoms-Mac-mini.local>
2020-07-17 12:56:44 +0700

    Test data

--- detect.py
+++ detect.py
@@ -130,6 +130,11 @@ def detect(save_img=False):
                     if save_img or view_img:  # Add bbox to image
                         label = '%s %.2f' % (names[int(cls)], conf)
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)])
+                        # Further processing of detection results
+                        # Get the Euler angles of this image
+                        
+                        # Perform optimization
+                        # Plot the localization result
 
             # Print time (inference + NMS)
             print('%sDone. (%.3fs)' % (s, t2 - t1))
